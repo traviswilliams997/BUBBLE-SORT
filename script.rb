015ec61fa7974a431a15170ce80a1883eb52d089
Travis Williams <traviswilliams997@gmail.com>
2022-06-11 13:37:20 -0500

    refactor: Create function to check if current number is bigger than next number

--- script.rb
+++ script.rb
@@ -7,12 +7,15 @@ end
 def check_if_index_out_of_bounds(array, index)
   index + 1 < array.length 
 end
+def   check_if_current_number_is_bigger_than_next_number(array, index)
+  array[index] > array[index + 1]
+end
 def bubble_sort(array)  
   (array.length - 1).times do 
     array.each_index do |index|
       
       #index out of bounds check and swap condition check 
-      if check_if_index_out_of_bounds(array, index) && array[index] > array[index + 1]
+      if check_if_index_out_of_bounds(array, index) &&  check_if_current_number_is_bigger_than_next_number(array, index)
         swap(array, index)
       end
     end
