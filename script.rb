ca4b766a791bb25ed32a847fb80983aa9d00b46e
Travis Williams <traviswilliams997@gmail.com>
2022-06-11 13:35:11 -0500

    refactor: Create function to check if index out of bounds

--- script.rb
+++ script.rb
@@ -4,12 +4,15 @@ def swap(array, index)
   array[index + 1] = array[index]
   array[index] = temp
 end
+def check_if_index_out_of_bounds(array, index)
+  index + 1 < array.length 
+end
 def bubble_sort(array)  
   (array.length - 1).times do 
     array.each_index do |index|
       
       #index out of bounds check and swap condition check 
-      if index + 1 < array.length && array[index] > array[index + 1]
+      if check_if_index_out_of_bounds(array, index) && array[index] > array[index + 1]
         swap(array, index)
       end
     end
