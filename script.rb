8f4b64e969bec902dde3482332ce8d82c4948610
Travis Williams <traviswilliams997@gmail.com>
2022-06-11 13:33:23 -0500

    refactor: Create function to swap numbers

--- script.rb
+++ script.rb
@@ -1,12 +1,16 @@
+def swap(array, index)
+  temp = 0
+  temp = array[index + 1]
+  array[index + 1] = array[index]
+  array[index] = temp
+end
 def bubble_sort(array)  
   (array.length - 1).times do 
     array.each_index do |index|
-      temp = 0
+      
       #index out of bounds check and swap condition check 
       if index + 1 < array.length && array[index] > array[index + 1]
-        temp = array[index + 1]
-        array[index + 1] = array[index]
-        array[index] = temp
+        swap(array, index)
       end
     end
   end
